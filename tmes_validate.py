6f3a5da1db4efc573aedcb249f11aa2a7024be01
Amedeo Fadini <fame@libero.it>
2020-03-16 21:06:04 +0100

    added sources example

---
+++ tmes_validate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import xarray as xr
+
+# check
+def check_time(file, date_start, shape):
+    '''
+    Check a netCdf or Grib forecast file against a required time interval
+    :param date_start: desired start date of file YYYMMDD format
+    :param shape: number of hourly time steps to have a valid file
+    :return: true if the file is valid
+    '''
+
+#dataset.variables['valid_time'][0].values
