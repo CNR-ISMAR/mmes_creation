5f92fa73c325596ec928ab0ac4df3414d660b414
Amedeo Fadini <amedeo.fadini@ve.ismar.cnr.it>
2021-10-27 02:08:44 +0200

    Python porting of preparation completed

--- mmes_functions.py
+++ mmes_functions.py
@@ -32,6 +32,7 @@ def prepare_forecast_sea_level(source, model, filename, filedate):
     :param filedate:
     :return: 0 or error
     """
+    variable = 'sea_level'
     proc_filename = os.path.splitext(os.path.basename(filename))[0] + '.nc'
     # change extension for tide model
     if model.system == 'tide':
@@ -65,7 +66,7 @@ def prepare_forecast_sea_level(source, model, filename, filedate):
             if miss != '':
                 tempfile = cdo.setmissval(miss, input=tempfile)
             # create dictionaries with variable to rename
-            new_vars  = Config['ensemble_variables'][model.variable]
+            new_vars  = Config['ensemble_variables'][variable]
             old_vars = model.var_names.split(',')
             if len(new_vars) == len(old_vars):
                 rdict = dict(zip(old_vars,new_vars))
@@ -99,10 +100,8 @@ def prepare_forecast_sea_level(source, model, filename, filedate):
         # mask before interpolation
         for s in steps['mask_before_interpolation']:
              if ms in s.keys():
-                 var = model.variable
-                 miss = model.miss_value
                  modmask = s[ms]
-                 cmd_arguments = ['ncap2', '-s', var + modmask + '=' + miss +'f;', tempfile]
+                 cmd_arguments = ['ncap2', '-s', modmask, tempfile]
                  try:
                      p = run(cmd_arguments)
                  except Exception as e:
@@ -126,10 +125,8 @@ def prepare_forecast_sea_level(source, model, filename, filedate):
         # mask_after_interpolation
         for s in steps['mask_after_interpolation']:
             if ms in s.keys():
-                var = model.variable
-                miss = model.miss_value
                 modmask = s[ms]
-                cmd_arguments = ['ncap2', '-s', var + modmask + '=' + miss + 'f;', tempfile]
+                cmd_arguments = ['ncap2', '-s', modmask , tempfile]
                 try:
                     p = run(cmd_arguments)
                 except Exception as e:
@@ -165,10 +162,8 @@ def prepare_forecast_waves(source, model, filename, filedate):
     :param filedate:
     :return: 0 or error
     """
+    variable = 'waves'
     proc_filename = os.path.splitext(os.path.basename(filename))[0] + '.nc'
-    # change extension for tide model
-    if model.system == 'tide':
-        proc_filename = os.path.splitext(os.path.basename(filename))[0] + '.tide'
     outputdir = os.path.join(data_dir, 'mmes_components', filedate)
     processedfile = os.path.join(outputdir, proc_filename)
     # create otuput dir if not exists
@@ -177,7 +172,7 @@ def prepare_forecast_waves(source, model, filename, filedate):
     if os.path.isfile(processedfile):
         print('prepared file exists, skipping')
         return 0
-    elif model.system == 'swanita':
+    else:
         # define dates
         date = datetime.strptime(filedate, "%Y%m%d").strftime("%Y-%m-%d")
         date2 = (datetime.strptime(date, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
@@ -187,20 +182,31 @@ def prepare_forecast_waves(source, model, filename, filedate):
         processing_opt = json.load(open(os.getcwd() + '/processing.json'))
         steps = processing_opt['waves_prepare']
         ms = model.system
-        # copy input file to tempfile and convert to NetCDF format
-        tempfile = cdo.copy(input=filename, options='-f nc')
+
         # step 0 merge downloaded components if needed steps['merge'components'] has a list of dictionaries widh model:system
         for st in steps['merge_components']:
             if ms in st.keys():
-                # get list of files with model system in the filename
+                # get list of files with model system and var in the filename
                 # to do the merge  must be equal to value setted in st[ms] from processing.json config
-                files = [os.path.join(outputdir, f) for f in os.listdir(outputdir)  if re.match(r'.+' + ms + '.+' + filedate + '.+', f)]
+                filedir = os.path.dirname(filename)
+                files = [os.path.join(filedir, f) for f in os.listdir(filedir)  if re.match(r'.+' + ms + '.+' + filedate + '.+', f)]
                 if len(files) == int(st[ms]):
-                    tempfile = cdo.merge(input=files)
+                    # replace vars in filename
+                    for v in model.var_names.split(','):
+                        filename = filename.replace('_' + v + '_', '_')
+                        processedfile = processedfile.replace('_' + v + '_', '_')
+                        # check if merged file was already processed
+                    if os.path.isfile(processedfile):
+                        print('prepared file exists, skipping')
+                        return 0
+                    cdo.merge(input=files,output=filename)
                 else:
                     msg = str(len(files)) + ' of ' + st[ms] + ' files present for model ' + ms
                     print(msg)
-        # step 0 extract wave variables in correct order
+                    return
+        # copy input file to tempfile and convert to NetCDF format
+        tempfile = cdo.copy(input=filename, options='-f nc')
+        # step 1 extract wave variables in correct order
         if ms in steps['variable_selection']:
             varlist = model.var_names
             tempfile = cdo.selvar(varlist, input=tempfile, options="-f nc")
@@ -208,39 +214,70 @@ def prepare_forecast_waves(source, model, filename, filedate):
             miss = model.miss_value
             if miss != '':
                 tempfile = cdo.setmissval(miss, input=tempfile)
-            # create dict with variable to rename
-            var = model.variable
-            rdict = {model.var_names: var}
-            # in place rename variables
-            cmd_arguments = ['ncrename', '-v', 'dslm,sea_level', tempfile]
-            try:
-                p = run(cmd_arguments)
-            except Exception as e:
-                print('error in rename variables: \n' + str(e))
-            # step 2 temporal interpolation
+            # create dictionaries with variable to rename
+            new_vars  = Config['ensemble_variables'][variable]
+            old_vars = model.var_names.split(',')
+            if len(new_vars) == len(old_vars) and new_vars != old_vars:
+                rdict = dict(zip(old_vars,new_vars))
+                cmd_arguments = ['ncrename']
+                for key, value in rdict.items():
+                    cmd_arguments.append('-v')
+                    cmd_arguments.append(key + ',' + value)
+                # in place rename variables
+                # example: cmd_arguments = ['ncrename', '-v', 'dslm,sea_level', tempfile]
+                cmd_arguments.append(tempfile)
+                try:
+                    p = run(cmd_arguments)
+                except Exception as e:
+                    print('error in rename variables: \n' + str(e))
+            else:
+                msg='Configuration error about var names of model ' + model.system
+        # step 2 temporal interpolation
         if ms in steps['temporal_interpolation']:
             # set time axis (already setted for most models
             tempfile = cdo.settaxis(date, "00:00:00", "1hour", input=tempfile)
             tempfile = cdo.inttime(date, "00:00:00", "1hour", input=tempfile)
+        # step 3 get only first 48 hours
         if ms in steps['get_48hours']:
             # Get fields in the 00-23 time range
             tempfile = cdo.seldate(date + "T00:00:00," + date2 + "T00:00:00", input=tempfile)
+        # set grid to unstructured
+        if ms in steps['set_grid_unstructured']:
+            us_gridfile = data_dir + '/config/weights/' +  ms + '.grid'
+            tempfile = cdo.setgrid(us_gridfile, input=tempfile)
+        # step 4 spatial interpolatio
+        if ms in steps['spatial_interpolation']:
+            maskfile = Config['mask_file']
+            int_gridfile = os.path.dirname(maskfile) + '/' + Config['ensemble_name'] + '_grid.txt'
+            if not os.path.isfile(int_gridfile):
+                write_grid(maskfile,int_gridfile)
+            weightfile = data_dir + '/config/weights/' + '_'.join([source.name,model.system, variable])
+            if not os.path.isfile(weightfile):
+                cdo.gendis(int_gridfile,input=tempfile,output=weightfile)
+            # use cdo remap
+            tempfile = cdo.remap(int_gridfile,weightfile,input=tempfile)
+        # extrapolate_missing values
+        if ms in steps['extrapolate_missing']:
+            tempfile = cdo.fillmiss(input=tempfile)
+        # step 5 mask_after_interpolation
+        for s in steps['mask_after_interpolation']:
+            if ms in s.keys():
+                modmask = s[ms]
+                cmd_arguments = ['ncap2', '-s', modmask , tempfile]
+                try:
+                    p = run(cmd_arguments)
+                except Exception as e:
+                    print('error in mask with ncap2: \n' + str(e))
+        # step 6 mask_outside_area
+        if ms in steps['mask_outside_area']:
+            maskfile = Config['mask_file']
+            tempfile = cdo.mul(input=[maskfile,tempfile], options='-O')
+        # step 7 removes value  equal 0
+        if ms in steps['remove_zero_values']:
+            cdo.setctomiss(0, input=tempfile)
+        # copy to destination
+        cdo.copy(input=tempfile,output=processedfile)
 
-
-
-    else:
-        # TODO port preparation in python
-        current_dir = os.getcwd()
-        script = current_dir + '/scripts/' + 'IWS_TMES_' + model.variable + '.sh'
-        if not os.path.isfile(script):
-            print('preparation script not found')
-            return
-        cmd_arguments = [script, filedate, model.system, filename, processedfile]
-        print(' '.join(cmd_arguments))
-        try:
-            p = run(cmd_arguments, check=True)
-        except ChildProcessError:
-            print('preaparation failed')
 
 def write_grid(maskfile, gridfile):
     cdo = Cdo()
